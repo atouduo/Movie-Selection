05909c24e6ee8171af59d2438189ba5ffca3b706
atouduo <aafan.nz@gmail.com>
2020-10-10 22:05:55 +1300

    changes

--- movie/adapters/repository.py
+++ movie/adapters/repository.py
@@ -17,12 +17,21 @@ class RepositoryException(Exception):
 class AbstractRepository(abc.ABC):
 
     @abc.abstractmethod
+    def add_director(self, director: Director):
+        """" Adds a User to the repository. """
+        raise NotImplementedError
+
+    def get_director(self) -> list:
+        """" Adds a User to the repository. """
+        raise NotImplementedError
+
+    @abc.abstractmethod
     def add_user(self, user: User):
         """" Adds a User to the repository. """
         raise NotImplementedError
 
     @abc.abstractmethod
-    def get_user(self, username) -> User:
+    def get_user(self) -> list:
         """ Returns the User named username from the repository.
 
         If there is no User with the given username, this method returns None.
@@ -35,73 +44,10 @@ class AbstractRepository(abc.ABC):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def get_movie(self, id: int) -> Movie:
-        """ Returns Article with id from the repository.
+    def get_movie(self) -> list:
+        """ Returns movie from the repository.
 
-        If there is no Article with the given id, this method returns None.
-        """
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def get_movie_by_date(self, target_date: date) -> List[Movie]:
-        """ Returns a list of Articles that were published on target_date.
-
-        If there are no Articles on the given date, this method returns an empty list.
-        """
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def get_number_of_movie(self):
-        """ Returns the number of Articles in the repository. """
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def get_first_movie(self) -> Movie:
-        """ Returns the first Article, ordered by date, from the repository.
-
-        Returns None if the repository is empty.
-        """
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def get_last_movie(self) -> Movie:
-        """ Returns the last Article, ordered by date, from the repository.
-
-        Returns None if the repository is empty.
-        """
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def get_movie_by_id(self, id_list):
-        """ Returns a list of Articles, whose ids match those in id_list, from the repository.
-
-        If there are no matches, this method returns an empty list.
-        """
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def get_movie_ids_for_tag(self, tag_name: str):
-        """ Returns a list of ids representing Articles that are tagged by tag_name.
-
-        If there are Articles that are tagged by tag_name, this method returns an empty list.
-        """
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def get_date_of_previous_movie(self, movie: Movie):
-        """ Returns the date of an Article that immediately precedes article.
-
-        If article is the first Article in the repository, this method returns None because there are no Articles
-        on a previous date.
-        """
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def get_date_of_next_movie(self, movie: Movie):
-        """ Returns the date of an Article that immediately follows article.
-
-        If article is the last Article in the repository, this method returns None because there are no Articles
-        on a later date.
+        If there is no Movie with the given name, this method returns None.
         """
         raise NotImplementedError
 
@@ -111,14 +57,10 @@ class AbstractRepository(abc.ABC):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def get_genre(self) -> List[Genre]:
+    def get_genre(self) -> list:
         """ Returns the Tags stored in the repository. """
         raise NotImplementedError
 
-    @abc.abstractmethod
-    def get_comments(self):
-        """ Returns the Comments stored in the repository. """
-        raise NotImplementedError
 
 
 
